7ad844b5b99a3d486b73780f1a2789940b7b51a8
quangtm198-pt <EMAIL_GITHUB_MI>
2025-12-11 21:42:37 +0700

    Mô tả ngắn thay đổi, ví dụ: Cập nhật giao diện Trang Chủ

--- app/api/diemdanh/route.ts
+++ app/api/diemdanh/route.ts
@@ -1,38 +1,19 @@
-// app/api/diemdanh/route.ts
 import { NextResponse } from "next/server";
 import { redis } from "@/lib/redis";
 
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { hoTen, donVi } = body;
+  const body = await req.json();
+  const { hoTen, donVi } = body;
 
-    if (!hoTen || !donVi) {
-      return NextResponse.json({ ok: false, message: "Thiếu dữ liệu" });
-    }
+  const KEY = "diemdanh:list";
+  await redis.rpush(KEY, JSON.stringify({ hoTen, donVi, time: Date.now() }));
 
-    // KEY lưu danh sách điểm danh
-    const KEY = "diemdanh:list";
-
-    // Lưu data vào Redis (dạng List)
-    await redis.rpush(KEY, JSON.stringify({ hoTen, donVi, time: Date.now() }));
-
-    return NextResponse.json({ ok: true });
-  } catch (err) {
-    console.error("Lỗi điểm danh:", err);
-    return NextResponse.json({ ok: false, message: "Lỗi server" });
-  }
+  return NextResponse.json({ ok: true });
 }
 
 export async function GET() {
-  try {
-    const KEY = "diemdanh:list";
-
-    const list = await redis.lrange(KEY, 0, -1);
-    const parsed = list.map((item) => JSON.parse(item));
-
-    return NextResponse.json({ ok: true, data: parsed });
-  } catch {
-    return NextResponse.json({ ok: false, data: [] });
-  }
+  const KEY = "diemdanh:list";
+  const list = await redis.lrange(KEY, 0, -1);
+  const data = list.map((x) => JSON.parse(x));
+  return NextResponse.json({ ok: true, data });
 }
