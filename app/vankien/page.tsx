7b242dcb4960cd515d2caf150a54810b3504899d
quangtm198-pt <EMAIL_GITHUB_MI>
2025-12-13 19:36:42 +0700

    Mô tả ngắn thay đổi, ví dụ: Cập nhật giao diện Trang Chủ

--- app/vankien/page.tsx
+++ app/vankien/page.tsx
@@ -6,19 +6,19 @@ export default function PageVanKien() {
   const documents = [
     {
       title: "1. Báo cáo chính trị của Ban Chấp hành Tỉnh đoàn (lâm thời) trình Đại hội",
-      link: "https://drive.google.com/file/d/1HHUKi6-eQGfIIUV8xRpO-vMUyFv_vW3S/view?usp=share_link", // thay bằng ID file Google Drive
+      link: "https://drive.google.com/file/d/15xrkcGSkoZyX_u3RmGFL4D8vKkT4FJ3a/view?usp=sharing", // thay bằng ID file Google Drive
     },
     {
       title: "2. Báo cáo kiểm điểm của Ban Chấp hành Tỉnh đoàn",
-      link: "https://drive.google.com/uc?export=view&id=DEF456",
+      link: "https://drive.google.com/file/d/15jOs_aeaPSn0X5Pq9c0JzWL2zeV_8KrP/view?usp=sharing",
     },
     {
       title: "3. Nghị quyết Đại hội",
-      link: "https://drive.google.com/uc?export=view&id=GHI789",
+      link: "https://drive.google.com/file/d/16nBUORoOk_-12qky7bX8DSrA_KjIg_77/view?usp=sharing",
     },
     {
       title: "4. Chương trình hành động thực hiện nghị quyết Đại hội",
-      link: "https://drive.google.com/uc?export=view&id=JKL012",
+      link: "https://drive.google.com/file/d/16nBUORoOk_-12qky7bX8DSrA_KjIg_77/view?usp=sharing",
     },
   ];
 
