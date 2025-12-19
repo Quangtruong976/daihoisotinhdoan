3df3c16f3798907a343681107a9a179eeaeb39d1
quangtm198-pt <EMAIL_GITHUB_MI>
2025-12-19 08:00:38 +0700

    Mô tả ngắn thay đổi, ví dụ: Cập nhật giao diện Trang Chủ

--- app/thamluan/page.tsx
+++ app/thamluan/page.tsx
@@ -1,4 +1,3 @@
-// app/thamluan/page.tsx
 import "../style.css";
 
 export default function PageThamLuan() {
@@ -6,113 +5,47 @@ export default function PageThamLuan() {
     {
       stt: 1,
       donvi: "Ban Tổ chức Tỉnh uỷ",
-      tieude: "Nâng cao chất lượng công tác bồi dưỡng, phát triển Đảng viên",
+      tieude:
+        "Nâng cao chất lượng công tác bồi dưỡng, phát triển Đảng viên từ tổ chức Đoàn TNCS Hồ Chí Minh",
       link: "https://drive.google.com/file/d/FILE_ID_1/view",
     },
     {
       stt: 2,
       donvi: "Sở Khoa học và Công nghệ",
-      tieude: "Thúc đẩy phong trào khởi nghiệp đổi mới sáng tạo trong thanh niên",
+      tieude:
+        "Tăng cường hỗ trợ và thúc đẩy phong trào khởi nghiệp đổi mới sáng tạo trong thanh niên",
       link: "https://drive.google.com/file/d/FILE_ID_2/view",
     },
-    {
-      stt: 3,
-      donvi: "Sở Nội vụ",
-      tieude: "Quản lý Nhà nước về thanh niên trên địa bàn tỉnh",
-      link: "https://drive.google.com/file/d/FILE_ID_3/view",
-    },
-  ];
-
-  const phanII = [
-    {
-      stt: 1,
-      donvi: "Đoàn TN Đảng ủy các cơ quan Đảng tỉnh",
-      tieude: "Vai trò ĐVTN trong bảo vệ nền tảng tư tưởng của Đảng",
-      link: "https://drive.google.com/file/d/FILE_ID_4/view",
-    },
-    {
-      stt: 2,
-      donvi: "Đoàn TN Bộ CHQS tỉnh",
-      tieude: "Thanh niên với nhiệm vụ bảo vệ nền tảng tư tưởng của Đảng",
-      link: "https://drive.google.com/file/d/FILE_ID_5/view",
-    },
-    {
-      stt: 3,
-      donvi: "Đoàn TNCS Hồ Chí Minh xã Bắc Ruộng",
-      tieude: "Giáo dục pháp luật, đạo đức, lối sống cho thanh niên",
-      link: "https://drive.google.com/file/d/FILE_ID_6/view",
-    },
-    // thêm đến 20 tham luận cũng KHÔNG bị tràn
   ];
 
   return (
-    <div className="main-container">
-      {/* PHẦN I */}
+    <div className="thamluan-wrapper">
       <h2 className="thamluan-title">
         I. Định hướng nhiệm vụ, giải pháp của các sở, ban, ngành
       </h2>
 
-      <div className="table-wrapper">
+      <div className="table-container">
         <table className="thamluan-table">
           <thead>
             <tr>
-              <th>TT</th>
-              <th>Sở/ngành</th>
-              <th>Tên bài</th>
-              <th>File</th>
+              <th className="col-stt">TT</th>
+              <th className="col-donvi">Sở/ngành</th>
+              <th className="col-tieude">Tên bài tham luận</th>
             </tr>
           </thead>
           <tbody>
             {phanI.map((item) => (
               <tr key={item.stt}>
-                <td className="col-stt">{item.stt}</td>
+                <td className="center">{item.stt}</td>
                 <td>{item.donvi}</td>
-                <td>{item.tieude}</td>
-                <td className="col-file">
+                <td>
                   <a
                     href={item.link}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="file-link"
                   >
-                    Xem
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      {/* PHẦN II */}
-      <h2 className="thamluan-title">
-        II. Tham luận của các cơ sở Đoàn trực thuộc Tỉnh đoàn
-      </h2>
-
-      <div className="table-wrapper">
-        <table className="thamluan-table">
-          <thead>
-            <tr>
-              <th>STT</th>
-              <th>Tên đơn vị</th>
-              <th>Tên bài tham luận</th>
-              <th>File</th>
-            </tr>
-          </thead>
-          <tbody>
-            {phanII.map((item) => (
-              <tr key={item.stt}>
-                <td className="col-stt">{item.stt}</td>
-                <td>{item.donvi}</td>
-                <td>{item.tieude}</td>
-                <td className="col-file">
-                  <a
-                    href={item.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="file-link"
-                  >
-                    Xem
+                    {item.tieude}
                   </a>
                 </td>
               </tr>
